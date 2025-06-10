0a3cf802d5b3da0aa50ced591e6e71509b901e55
kaouther <baramakaouther@gmail.com>
2025-06-10 04:40:22 -0700

    project4

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+// next.config.js
+module.exports = {
+  output: "export", // Enables static export
+  basePath: process.env.NODE_ENV === "production" ? "/your-repo-name" : "", // Needed if deploying to GitHub Pages in a subpath
+  assetPrefix: process.env.NODE_ENV === "production" ? "/your-repo-name/" : "", // For assets (CSS, images, etc.)
+};
 
 export default nextConfig;
